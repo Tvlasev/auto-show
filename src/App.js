792c7002e41714932d7bb9b69b204da211c87256
Todor Vlasev <todor.vlasev@asml.com>
2020-09-22 04:47:08 +0300

    minor

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Home from "./components/Home/Home";
 
-function App() {
+const App = () => {
   return (
     <div>
       <Switch>
